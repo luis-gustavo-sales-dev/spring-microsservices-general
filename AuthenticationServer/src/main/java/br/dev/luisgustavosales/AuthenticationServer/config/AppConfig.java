b36b283bf5831c945be3f7c1ce3fcfdc5a655f6c
Luis Gustavo Sales Dev <luisgssf@gmail.com>
2022-06-15 19:10:23 -0300

    Configure beans to create tokens.

--- AuthenticationServer/src/main/java/br/dev/luisgustavosales/AuthenticationServer/config/AppConfig.java
+++ AuthenticationServer/src/main/java/br/dev/luisgustavosales/AuthenticationServer/config/AppConfig.java
@@ -1,16 +1,33 @@
 package br.dev.luisgustavosales.AuthenticationServer.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
 
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey(jwtSecret);
+		return tokenConverter;
+	}
+	
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
 	}
 
 	//@SuppressWarnings("deprecation")
