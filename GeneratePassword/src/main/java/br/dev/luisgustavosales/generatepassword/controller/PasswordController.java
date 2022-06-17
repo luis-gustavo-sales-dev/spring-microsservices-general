659c518b633ff385222b5a7b385281f2aa022586
Luis Gustavo Sales Dev <luisgssf@gmail.com>
2022-06-17 16:54:07 -0300

    A bean to control access to Passwords was created.

--- GeneratePassword/src/main/java/br/dev/luisgustavosales/generatepassword/controller/PasswordController.java
+++ GeneratePassword/src/main/java/br/dev/luisgustavosales/generatepassword/controller/PasswordController.java
@@ -9,42 +9,53 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.dev.luisgustavosales.generatepassword.config.Utilities;
 import br.dev.luisgustavosales.generatepassword.entities.PasswordInfo;
 import br.dev.luisgustavosales.generatepassword.services.PasswordService;
 
 @RestController("passwords")
 public class PasswordController {
-	
+
 	@Autowired
 	private PasswordService passwordService;
-	
+
+	@Autowired
+	private Utilities utilities;
+
 	@GetMapping("/{id}")
-	public ResponseEntity<PasswordInfo> findById(
-			@PathVariable Long id, 
-			@RequestHeader Map<String, String> headers) {
-		
-		/*headers.forEach((key, value) -> {
-	        System.out.println(String.format("Header '%s' = %s", key, value));
-	    });*/
-		
+	public ResponseEntity<PasswordInfo> findById(@PathVariable Long id, @RequestHeader Map<String, String> headers) {
+
+		/*
+		 * headers.forEach((key, value) -> {
+		 * System.out.println(String.format("Header '%s' = %s", key, value)); });
+		 */
+
 		var passwordInfo = passwordService.findPasswordInfoById(id);
 		if (passwordInfo == null) {
-			System.out.println("PasswordController: " + passwordInfo);
+			// System.out.println("PasswordController: " + passwordInfo);
 			return ResponseEntity.notFound().build();
 		}
 		
-		if (!passwordInfo.getUsername().equals(headers.get("username"))) {
-			
-			/*System.out.println("PasswordController: passwordInfo.Username" + 
-					passwordInfo.getUsername() + " Username: " +
-					headers.get("username"));
-					*/
-			
+		/*System.out.println(
+				utilities.canAcessPasswordResource(
+					passwordInfo.getUsername(), 
+					headers.get("username")));
+		 */
+		
+		if (!utilities.canAcessPasswordResource(
+				passwordInfo.getUsername(), 
+				headers.get("username"))) {
+
+			/*
+			 * System.out.println("PasswordController: passwordInfo.Username" +
+			 * passwordInfo.getUsername() + " Username: " + headers.get("username"));
+			 */
+
 			// Must throws an exception
 			return ResponseEntity.notFound().build();
 		}
-		
+
 		return ResponseEntity.ok(passwordInfo);
-		
+
 	}
 }
